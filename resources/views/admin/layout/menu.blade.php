b705eaf0d00d6ba21a874da77d2c98ecd47f858d
dangphuchuu <phuchuu0120@gmail.com>
2022-10-22 19:04:47 +0700

    chinh thuc

---
+++ resources/views/admin/layout/menu.blade.php
@@ -0,0 +1,89 @@
+<nav class="pcoded-navbar">
+    <div class="nav-list">
+        <div class="pcoded-inner-navbar main-menu">
+            <div class="pcoded-navigation-label">Manage</div>
+            <ul class="pcoded-item pcoded-left-item">
+                <!-- Category -->
+                <li class="pcoded">
+                    <a href="admin/categories/list" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-menu rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Categories</span>
+                    </a>
+                </li>
+                <!-- Products -->
+                <li class="pcoded">
+                    <a href="admin/products/list" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-package rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Products</span>
+                    </a>
+                </li>
+                <!-- Videos -->
+                <li class="pcoded">
+                    <a href="admin/videos/list" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                        <i class="feather icon-play-circle rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Videos</span>
+                    </a>
+                </li>
+                <!-- Profiles -->
+                <li class="pcoded">
+                    <a href="admin/profiles/list" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-user rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Profiles</span>
+                    </a>
+                </li>
+                <!-- Logo -->
+                <li class="pcoded">
+                    <a href="admin/logos" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-upload  rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Logos</span>
+                    </a>
+                </li>
+                <!-- Banner -->
+                <li class="pcoded">
+                    <a href="admin/banners" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-cpu rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Banners</span>
+                    </a>
+                </li>
+                <!-- Footer -->
+                <li class="pcoded">
+                    <a href="admin/footers" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-aperture rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Footers</span>
+                    </a>
+                </li>
+                <li class="pcoded">
+                    <a href="admin/charity/list" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-activity rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Charity</span>
+                    </a>
+                </li>
+                <!-- Htradexim -->
+                <li class="pcoded">
+                    <a href="/" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                            <i class="feather icon-eye rotate-refresh"></i>
+                        </span>
+                        <span class="pcoded-mtext">Htradexim</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
